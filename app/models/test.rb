aa65e35275d7b191571ea6899d7b1717ed8d2267
Lushnikov-N-A <newu1986@yandex.ru>
2022-07-31 14:43:34 +0300

    Lesson 5, Bug fix 31.07.22

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,9 +6,9 @@ class Test < ApplicationRecord
   has_many :author, class_name: 'User', foreign_key: :author_id
   has_many :users, through: :test_passings
 
-  belongs_to :user, optional: true
-  belongs_to :author, class_name: 'User',optional: true
-  belongs_to :category, optional: true
+  belongs_to :user
+  belongs_to :author, class_name: 'User'
+  belongs_to :category
 
   def self.sort_tests_by_category(test_category)
     joins(:category)
