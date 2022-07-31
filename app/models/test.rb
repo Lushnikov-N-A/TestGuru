88bdc64a107466546dce5e326a46c2650aad7158
Lushnikov-N-A <newu1986@yandex.ru>
2022-07-31 20:09:05 +0300

    Lesson 5, Bug fix_2 31.07.22

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  has_many :questions, dependent: :destroy
-  has_many :test_passings, dependent: :destroy
-  has_many :author, class_name: 'User', foreign_key: :author_id
-  has_many :users, through: :test_passings
-
   belongs_to :user
   belongs_to :author, class_name: 'User'
   belongs_to :category
+
+  has_many :questions, dependent: :destroy
+  has_many :test_passings, dependent: :destroy
+  has_many :users, through: :test_passings
 
   def self.sort_tests_by_category(test_category)
     joins(:category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@
 
 class User < ApplicationRecord
   has_many :test_passings, dependent: :destroy
+  has_many :tests, through: :test_passings
   has_many :tests_author, class_name: 'Test', foreign_key: :author_id
 
-  belongs_to :test
-
   def test_level_pass(level_pass_test)
-    Test.joins('INNER JOIN test_passings ON tests.id = test_passings.test_id').where(level: level_pass_test)
+    Test.joins('INNER JOIN test_passings ON tests.id = test_passings.test_id')
+    .where(test_passings: { user_id: id}, level: level_pass_test)
   end
 end
